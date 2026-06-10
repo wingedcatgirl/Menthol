9c1d3e87d9df5d8436c50e3f77bdd72b4d7a2c34
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-06-10 14:45:12 -0400

    for later

--- tags/menu.lua
+++ tags/menu.lua
@@ -18,8 +18,17 @@ SMODS.Tag{
         G.CONTROLLER.locks[lock] = true
         tag:yep("+", HEX("CA7CA7"), function()
             local key = 'p_minty_treat_normal_1'  --..(math.random(1,2))
+
             local card = Card(G.play.T.x + G.play.T.w/2 - G.CARD_W*1.27/2,
             G.play.T.y + G.play.T.h/2-G.CARD_H*1.27/2, G.CARD_W*1.27, G.CARD_H*1.27, G.P_CARDS.empty, G.P_CENTERS[key], {bypass_discovery_center = true, bypass_discovery_ui = true})
+
+            --[[ Doing it this way causes it to appear in the wrong size and location, todo find a way around that?
+            local card = SMODS.create_card{
+                set = "Booster",
+                key = key,
+                bypass_discovery_center = true
+            }
+            --]]
             card.cost = 0
             card.from_tag = true
             G.FUNCS.use_card({config = {ref_table = card}})
